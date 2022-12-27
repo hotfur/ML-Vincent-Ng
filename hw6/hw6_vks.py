b936eb6397f5732c617c3d23618d316022a9b517
Vuong Kha Sieu <sieu.vuong.200087@student.fulbright.edu.vn>
2022-12-28 04:15:19 +0700

    Add HW6 gamma,epsilon

--- hw6/hw6_vks.py
+++ hw6/hw6_vks.py
@@ -10,23 +10,51 @@ def main():
     trans = np.concatenate((np.loadtxt(sys.argv[5], dtype=np.float64), np.zeros(n)[None, :]), axis=0)
     emit = np.concatenate((np.loadtxt(sys.argv[6], dtype=np.float64), np.zeros(m)[None, :]), axis=0)
     data = np.char.strip(a=np.loadtxt(sys.argv[7], dtype=str), chars='o').astype(int)
+    seq = data[0]
+    alpha = forward(n, start, trans, emit, data[0])
+    beta = backward(n, start, trans, emit, data[0])
 
-    i = 0
 
-
-def forward(n, start, emit, seq):
-    list_alpha = np.empty(shape=(len(seq),n))
-    list_alpha[0] = start*emit[:, seq[0]]
+def calc_epsilon(n, start, trans, emit, seq):
+    # Calculate alpha
+    alpha = np.empty(shape=(len(seq) + 1, n))
+    alpha[-1] = np.zeros(n)
+    alpha[-1][-1] = 1
+    alpha[0] = start * emit[:, seq[0]]
     for i in range(1, len(seq)):
-        list_alpha[i] = list_alpha[-1]*emit[:]
-    return list_alpha
+        alpha[i] = (trans @ alpha[i - 1]) * emit[:, seq[i]]
+    # Calculate beta
+    beta = np.empty(shape=(len(seq) + 1, n))
+    beta[-1] = np.zeros(n)
+    beta[-1][-1] = 1
+    for i in range(len(seq) - 1, -1, -1):
+        beta[i] = (trans @ beta[i + 1]) * emit[:, seq[i]]
+    # Calculate gamma
+    ab_product = alpha * beta
+    gamma = ab_product / np.sum(ab_product, axis=1)[:, None]
+    # Calculate epsilon
+    epsilon = np.empty(shape=(len(seq), n, n))
+    for t in range(0, len(seq)):
+        layer = trans * (alpha[t][:, None] * beta[t + 1] * emit[:, seq[t + 1]])
+        epsilon[t] = layer/np.sum(layer)
+    return gamma, epsilon
 
-def backward(n, start, emit, seq):
-    list_beta = np.empty(shape=(len(seq),n))
-    list_beta[0] = np.zeros(n)
+def forward(n, start, trans, emit, seq):
+    alpha = np.empty(shape=(len(seq)+1, n))
+    alpha[-1] = np.zeros(n)
+    alpha[-1][-1] = 1
+    alpha[0] = start*emit[:, seq[0]]
     for i in range(1, len(seq)):
-        list_beta[i] = list_beta[-1]*emit[:]
-    return list_beta
+        alpha[i] = (trans@alpha[i-1])*emit[:, seq[i]]
+    return alpha
+
+def backward(n, start, trans, emit, seq):
+    beta = np.empty(shape=(len(seq)+1, n))
+    beta[-1] = np.zeros(n)
+    beta[-1][-1] = 1
+    for i in range(len(seq)-1, -1, -1):
+        beta[i] = (trans@beta[i+1])*emit[:, seq[i]]
+    return beta
 
 if __name__ == '__main__':
     main()
