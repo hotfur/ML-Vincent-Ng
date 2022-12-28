69cca9463b2ae45d480c45a78dbcef32eb22350a
Vuong Kha Sieu <sieu.vuong.200087@student.fulbright.edu.vn>
2022-12-28 17:10:13 +0700

    Done HW6 but not correct

--- hw6/hw6_vks.py
+++ hw6/hw6_vks.py
@@ -5,21 +5,24 @@ import numpy as np
 import sys
 import multiprocessing
 
+
 def main():
+    # Dataloader and preprocessing to reduce problem to typical HMM problem
     n, m, iterations = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
     start = np.loadtxt(sys.argv[4], dtype=np.longdouble)
     trans = np.concatenate((np.loadtxt(sys.argv[5], dtype=np.longdouble), np.zeros(n)[None, :]), axis=0)
     emit = np.concatenate((np.loadtxt(sys.argv[6], dtype=np.longdouble), np.zeros(m)[None, :]), axis=0)
-    data = np.char.strip(a=np.loadtxt(sys.argv[7], dtype=str), chars='o').astype(int)-1
+    data = np.char.strip(a=np.loadtxt(sys.argv[7], dtype=str), chars='o').astype(int) - 1
     num_seq = data.shape[0]
-
+    # Open multiprocessor pool to execute task in parallel
     pool = multiprocessing.Pool()
     for i in range(iterations):
+        # Calculate the variables of each sequence parallely
         args = []
         for seq in data:
             args.append((n, m, start, trans, emit, seq))
         results = pool.starmap(calc_seq, args)
-
+        # Appends calculated matrices to the main 3d tensors, then reduce the tensor to a matrix
         pi, a_numer, ab_denom, b_numer = np.zeros((num_seq, start.shape[0])), \
             np.zeros((num_seq, trans.shape[0], trans.shape[1])), np.zeros((num_seq, n)), \
             np.zeros((num_seq, emit.shape[0], emit.shape[1]))
@@ -29,12 +32,26 @@ def main():
         a_numer = np.sum(a_numer, axis=0)
         ab_denom = np.sum(ab_denom, axis=0)
         b_numer = np.sum(b_numer, axis=0)
-
-        start = pi/num_seq
-        trans = a_numer/ab_denom[:,None]
-        emit = b_numer/ab_denom[:,None]
-    print("fone")
+        # Calculate final model parameters based on reduced tensors
+        start = pi / num_seq
+        trans = a_numer / ab_denom[:, None]
+        emit = b_numer / ab_denom[:, None]
+        # Printer
+        print("After iteration %d:" % (i + 1))
+        for k in range(start.shape[0]):
+            print("pi_%d: %4.4f " % (k + 1, start[k]), end='')
+        print()
+        for row in range(trans.shape[0] - 1):
+            for col in range(trans.shape[1]):
+                print("a_%d%d: %4.4f " % (row + 1, col + 1, trans[row, col]), end='')
+            print()
+        for row in range(emit.shape[0] - 1):
+            for col in range(emit.shape[1]):
+                print("b_%d(o%d): %4.4f " % (row + 1, col + 1, emit[row, col]), end='')
+            print()
     pool.close()
+    return 0
+
 
 def calc_seq(n, m, start, trans, emit, seq):
     # Calculate alpha
@@ -56,20 +73,20 @@ def calc_seq(n, m, start, trans, emit, seq):
     # Calculate epsilon
     epsilon = np.empty(shape=(len(seq), n, n))
     for t in range(0, len(seq)):
-        if t==len(seq)-1:
+        if t == len(seq) - 1:
             layer = trans * (alpha[t][:, None] * beta[t + 1])
         else:
             layer = trans * (alpha[t][:, None] * beta[t + 1] * emit[:, seq[t + 1]])
-        epsilon[t] = layer/np.sum(layer)
+        epsilon[t] = layer / np.sum(layer)
     # Calculate qualities for return
     pi = gamma[0]
     a_numer = np.sum(epsilon, axis=0)
     ab_denom = np.sum(gamma, axis=0)
     # Calculate the indicator mask
-    indicator = np.zeros(shape=(len(seq)+1, m))
+    indicator = np.zeros(shape=(len(seq) + 1, m))
     for i in range(len(seq)):
         indicator[i, seq[i]] = 1
-    b_numer = gamma.T@indicator
+    b_numer = gamma.T @ indicator
     return pi, a_numer, ab_denom, b_numer
 
 
